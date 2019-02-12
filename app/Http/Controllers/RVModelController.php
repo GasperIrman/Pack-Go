34a1b060625193605fce85ce17ac636cc94f4b98
Gašper Irman <gasper.irman@gmail.com>
2019-02-12 09:23:59 +0100

    Revert "Revert "Added db tables""
    This reverts commit 1f764b32c9529ab517628886ca44dfc833f6e42c.

---
+++ app/Http/Controllers/RVModelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RVModel;
+use Illuminate\Http\Request;
+
+class RVModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RVModel  $rVModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RVModel $rVModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RVModel  $rVModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RVModel $rVModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RVModel  $rVModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RVModel $rVModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RVModel  $rVModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RVModel $rVModel)
+    {
+        //
+    }
+}
