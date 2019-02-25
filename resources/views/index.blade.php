434584b27ec7c25cfd708176172fac31620c16e6
Valentin Grudnik <wallicar99@gmail.com>
2019-02-25 12:30:43 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,8 @@
         <div class="card">
           <img class="card-img-top" src="..." alt="Card image cap">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">Card title</h5>  
+            
             <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
           </div>
