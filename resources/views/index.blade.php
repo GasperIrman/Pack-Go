e58d4a180629f9a82cd70348485020626c322a6f
Valentin Grudnik <wallicar99@gmail.com>
2019-02-25 12:57:32 +0100

    Navbar add

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,6 +27,21 @@
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
           </div>
         </div>
-      </div>
+ </div>
 
+ <div class="jumbotron jumbotron-fluid" style=" margin-top: 20px; background-color: white;">
+    <div class="container">
+      <h1 class="display-4">Fluid jumbotron</h1>
+      <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    </div>
+  </div>
+  <div class="card" >
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
     @endsection
