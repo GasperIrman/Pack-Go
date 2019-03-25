a114227f069da7457a3c83c5020ca06136df5776
Gašper Irman <gasper.irman@gmail.com>
2019-03-25 13:47:08 +0100

    form fix v2

--- resources/views/motorhomes/search.blade.php
+++ resources/views/motorhomes/search.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 <!-- tle se nek filter navbar ko se expanda -->
 @section('content')
-<div class="" id="parentFilter" style="background-color: #a0a0a0; margin-top: -25px; padding: 15px; width: auto;overflow: hidden;border-radius: 15px">
+<div class="" id="parentFilter" style="background-color: lightgrey; margin-top: -20px; padding: 15px; width: auto; overflow: hidden; border-radius: 15px; border: black 1px solid">
   <!-- ko bos gor kliknu se bo filter bar expandu cez sirino ekrana pa mal se navzdol pa bos meu opcije za filtre -->
 <div id="filter" class="btn">
   Filter
@@ -11,30 +11,30 @@
   <div class="row">
     <div class="col-md-3 col-sm-3">
       {{ Form::label('Model / Description') }}
-        {{ Form::text('search', '', ['placeholder' => 'Search', 'onkeyup' => 'showResult(this.value, "#liveModel")', 'style' => 'display: block;', 'autocomplete' => 'off', 'id' => 'Models']) }}
-        <div id="liveModel" class="dropdown-content" style="width: 160px; position: absolute; margin-left: 0vw; background-color: white; border-radius: 5px; z-index: 5"></div>
+        {{ Form::text('search', '', ['class' => 'form-control', 'placeholder' => 'Search', 'onkeyup' => 'showResult(this.value, "#liveModel")', 'style' => 'display: block;', 'autocomplete' => 'off', 'id' => 'Models']) }}
+        <div id="liveModel" class="dropdown-content" style="width: 90%; position: absolute; margin-left: 0vw; background-color: white; border-radius: 5px; z-index: 5"></div>
     </div>
 
     <div class="col-md-3 col-sm-3">
       {{ Form::label('Country') }}
-      {{ Form::text('cntry', '', ['placeholder' => 'Search', 'onkeyup' => 'showResult(this.value, "#liveCountry")', 'style' => 'display: block;', 'autocomplete' => 'off', 'id' => 'Countries']) }}
-      <div id="liveCountry" class="dropdown-content" style="width: 160px; position: absolute; margin-left: 0vw; background-color: white; border-radius: 5px; z-index: 5"></div>
+      {{ Form::text('cntry', '', ['class' => 'form-control', 'placeholder' => 'Search', 'onkeyup' => 'showResult(this.value, "#liveCountry")', 'style' => 'display: block;', 'autocomplete' => 'off', 'id' => 'Countries']) }}
+      <div id="liveCountry" class="dropdown-content" style="width: 90%; position: absolute; margin-left: 0vw; background-color: white; border-radius: 5px; z-index: 5"></div>
     </div>
 
     <div class="col-md-3 col-sm-3">
       {{ Form::label('City') }}
-      {{ Form::text('city', '', ['placeholder' => 'Search', 'onkeyup' => 'showResult(this.value, "#liveCity")', 'style' => 'display: block;', 'autocomplete' => 'off', 'id' => 'Cities']) }}
-      <div id="liveCity" class="dropdown-content" style="width: 160px; position: absolute; margin-left: 0vw; background-color: white; border-radius: 5px; z-index: 5"></div>
+      {{ Form::text('city', '', ['class' => 'form-control', 'placeholder' => 'Search', 'onkeyup' => 'showResult(this.value, "#liveCity")', 'style' => 'display: block;', 'autocomplete' => 'off', 'id' => 'Cities']) }}
+      <div id="liveCity" class="dropdown-content" style="width: 90%; position: absolute; margin-left: 0vw; background-color: white; border-radius: 5px; z-index: 5"></div>
     </div>
 
     <div class="col-md-3 col-sm-3">
       {{ Form::label('Bed number') }}
-    {{ Form::text('beds', '', ['placeholder' => 'Search', 'style' => 'display: block']) }}
+    {{ Form::text('beds', '', ['class' => 'form-control', 'placeholder' => 'Search', 'style' => 'display: block']) }}
     </div>
   </div>
-  <div class="row">
+  <div class="row" style="margin-top: 10px">
     <div class="col-md-3 col-sm-3">
-      {{ Form::submit('Search', ['class' => 'btn btn-primary', 'style' => 'display: block'])}}
+      {{ Form::submit('Search', ['class' => 'btn btn-outline-dark', 'style' => 'display: block; '])}}
     </div>
   </div>
   {{ Form::close() }}
@@ -104,7 +104,6 @@
     if(value.length == 0)
     {
       $(id).html('');
-      $(id).css('border', '');
       return;
     }
     console.log(id);
@@ -114,7 +113,6 @@
       {
         //$('#live').html('<select style="width: 159px; z-index: 1; position: relative">' + this.responseText + '</select>');
         $(id).html(this.responseText);
-        $(id).css('border', 'solid black 1px');
         console.log(this.responseText);
       } 
     }
@@ -134,19 +132,16 @@
     if(id.startsWith('Model'))
     {
       $('#Models').val($('#'+id).val());
-      $('#'+id).parent().css('border', '');
       $('#'+id).parent().html('');
     }
     if(id.startsWith('Country'))
     {
       $('#Countries').val($('#'+id).val());
-      $('#'+id).parent().css('border', '');
       $('#'+id).parent().html('');
     }
     if(id.startsWith('City'))
     {
       $('#Cities').val($('#'+id).val());
-      $('#'+id).parent().css('border', '');
       $('#'+id).parent().html('');
       
     }
