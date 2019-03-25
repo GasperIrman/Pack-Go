c7d44fa77ea3a1cf981f49bfa11caa6289e1a9b6
Valentin Grudnik <wallicar99@gmail.com>
2019-03-25 20:49:03 +0100

    User  show/edit style change

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,18 +2,35 @@
 
 @section('content')
 <div class="container">
+        <div class="row">
+                <div class="col-md-2 col-sm-2 ">
+                        <div class="profile-userpic">
+                                <img src="/storage/profile_pictures/{{$user->pic_url}}" alt="" style=" width: 150px;
+                                height: 150px;
+                                border-radius: 50%;
+                                margin: 20px;
+                              
+                                object-fit: cover;
+                                object-position: center right;" alt="Profilna slika">
+                            </div>
+                </div>
+                <div class="col-md-2 col-sm-2 ">
+                </div>
+                <div class="col-md-8 col-sm-8 ">
     {!! Form::open(['method' => 'POST', 'action' => ['UserController@update', $user->id]]) !!}
     {{ Form::hidden('_method', 'PUT')}}
     {{ Form::label('Name') }}
-    {{ Form::text('name', $user->name) }}<br>
-    {{ Form::button('Change Password', ['id' => 'PwrdBtn']) }}
+    {{ Form::text('name', $user->name) }} <br>
+    {{ Form::label('Address') }}
+    {{ Form::text('address', $user->email) }}
     {{ Form::text('password', $user->password, ['style' => 'visibility: hidden', 'id' => 'asd']) }}<br>
     {{ Form::label('Profile picture') }}
     {{ Form::file('image') }}<br>
-    {{ Form::label('Address') }}
-    {{ Form::text('address', $user->email) }}<br>
-    {{ Form::submit('Update', ['class' => 'btn btn-primary'])}}
+    {{ Form::label('Change Password:') }} 
+     {{ Form::button('Change Password', ['id' => 'PwrdBtn']) }} <br><br>
+    {{ Form::submit('Update', ['class' => 'btn btn-outline-dark'])}}
     {!! Form::close() !!}
+     </div>
 </div>
 <script>
     $('#PwrdBtn').click(function(){
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,21 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-Ttmu kurcu je ime {{$user->name}}<br>
-Tt kurac ma takle mail {{$user->email}}<br>
-profilka {{$user->pic_url}}<br>
+<div class="profile-userpic">
+		<img src="/storage/profile_pictures/{{$user->pic_url}}" alt="" style=" width: 150px;
+		height: 150px;
+		border-radius: 50%;
+		margin: 20px;
+	  
+		object-fit: cover;
+		object-position: center right;" alt="Profilna slika">
+	</div><br>
+Name: {{$user->name}}<br>
+Mail: {{$user->email}}<br>
+
 @if($user->provider)
-	@foreach($user->motorhome->sortByDesc('created_at') as $rv)
-	<div class="jumbotron jumbotron-fluid" style="overflow: hidden">
-		<div class="col-md-4 col-sm-4">
-                            <img style="width:100%" src="/storage/cover_images/{{$rv->cover_image}}">
-                        </div>
-		<div class="container" style="margin-top: 20vh;float: right; width: 30%">
-			<div class="lead">
-				{{ $rv->description }}
-			</div>
-		</div>
-	</div>
-	@endforeach
-@endif
+        @foreach ($user->motorhome->sortByDesc('created_at') as $rv)
+        <div class="jumbotron jumbotron-fluid" style=" margin-top: 20px; background-color: white; border-radius: 17px; border: solid 1px black">
+                <div class="container">
+                        <div class="well">
+                                <div class="row">
+                               <div class="col-md-8 col-sm-8">
+                                   <img style="width:100%" src="/storage/cover_images/{{$rv->cover_image}}">
+                               </div>
+                               <div class="col-md-4 col-sm-4">
+                                <h2>{{$rv->model->name}}</h2>
+                               <h5>DESCRIPTION:</h5> <br>
+                                <p>{{$rv->description}}</p>
+                                <h5>DETAILS:</h5>
+								<a href="/motorhomes/{{$rv->id}}"><button type="button" class="btn btn-outline-dark" >VIEW</button></a><br><br>
+								<p>Uploaded at: 
+										{!!date('G:i d.m.Y', strtotime($rv->created_at))!!}</p>
+                           </div>
+                       </div>
+                   </div>  </div>
+              </div>
+        
+    
+        @endforeach
+       
+    @else
+        <p>No motorhomes found</p>
+	@endif
+	
+
 @endsection
